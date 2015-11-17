127bec44be52eb2c726373f6828bb2eedab89c48
Dylan Tenebruso <dtenebruso92@gmail.com>
2015-11-17 15:03:09 -0500

    Create csvreader.py
    A Python csv reader I made for practice. It reads in the data from the csv file and prints it all out. If you want to search for sales numbers of a specific color and their respective dates, the program will find all instances of that color and append the indices to a list and then iterate through that list to the color,sales quantity and date lists according to their respective indices.
    
    Made using this video: https://www.youtube.com/watch?v=K_oXb04izZM
    and big help from the answer on this: http://stackoverflow.com/questions/6294179/how-to-find-all-occurrences-of-an-element-in-a-list

---
+++ csvreader.py
@@ -0,0 +1,31 @@
+import csv
+
+with open('example.csv') as csvfile:
+	readCSV = csv.reader(csvfile, delimiter =',')
+
+	dates = []
+	colors = []
+	sales = []
+
+	for row in readCSV:
+		date = row[0]
+		color = row[2]
+		saleNum = row[3]
+
+		dates.append(date)
+		colors.append(color)
+		sales.append(saleNum)
+
+	print(dates)
+	print(colors)
+	print(sales)
+
+	coldex = []
+	searchVal = input('What color would you like to see sales numbers for? ')
+	coldex = [i for i, x in enumerate(colors) if x == searchVal]
+		
+	for i in coldex:
+		salesDate = dates[i]
+		salesColor = colors[i]
+		salesQuant = sales[i]
+		print('The number of sales for the color ', searchVal,' is ',salesQuant, ' on', salesDate)
