36bc269e7d611168a5be6da215683977fb5ea24c
Dylan Tenebruso <dtenebruso92@gmail.com>
2015-11-19 22:28:14 -0500

    Create banner.py
    creates a banner. found a python challenge site. might do a couple ore this was challenge #1. Happy to say it took m maybe 5 minutes...not even. Maybe im finally getting this programming thing>>

---
+++ banner.py
@@ -0,0 +1,16 @@
+def banner(uName):
+	ban_length = len(uName)
+
+	new_length = ban_length +6
+	lines = ("*"*new_length)
+
+	print(lines)
+	print("\n.::"+uName+"::.\n")
+	print(lines)
+
+
+
+
+name = input("What is your name? ")
+
+banner(name)
